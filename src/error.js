819fd9b4f7b4a39474d652372c43f1e644909b80
Justin Stayton <jstayton@users.noreply.github.com>
2023-10-30 11:45:16 -0400

    Use JSDoc for documentation

--- src/error.js
+++ src/error.js
@@ -1,4 +1,15 @@
+/**
+ * The custom error thrown when verification fails.
+ *
+ * @memberof module:@truepic/webhook-verifier
+ * @extends Error
+ */
 class TruepicWebhookVerifierError extends Error {
+  /**
+   * Create an error for a failed verification.
+   *
+   * @param {string} message The description of what failed.
+   */
   constructor(message) {
     super(message)
 
