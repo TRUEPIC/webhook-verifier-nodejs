11ae756f738d0e23bcdae99d1750d6c1c3847cb5
Justin Stayton <jstayton@users.noreply.github.com>
2026-08-19 12:48:11 -0400

    Fix eslint-plugin-n import

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 const js = require('@eslint/js')
 const { defineConfig } = require('eslint/config')
 const prettier = require('eslint-config-prettier')
-const node = require('eslint-plugin-n').default
+const node = require('eslint-plugin-n')
 const globals = require('globals')
 
 module.exports = defineConfig([
